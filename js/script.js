730ce3c9a0fd2c7efe9a69b64256e1a89f533054
kirill <kirillbasenko6@gmail.com>
2022-05-20 13:46:34 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /*function makeAdder(x){
             function add(y){
                return y + x;
@@ -792,19 +792,19 @@ function Clock({ template }) {
 
 class Clock{
    constructor({template}){
-      this.template = template
+      this.template = template;
    }
    render(){
       let date = new Date();
 
       let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
+      if (hours < 10) {hours = '0' + hours;}
 
       let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
+      if (mins < 10) {mins = '0' + mins;}
 
       let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
+      if (secs < 10) {secs = '0' + secs;}
 
       let output = this.template
          .replace('h', hours)
@@ -814,11 +814,12 @@ class Clock{
       console.log(output);
    }
    stop(){
-      clearInterval(timer)
+      let timer;
+      clearInterval(timer);
    }
    start(){
-      this.render()
-      this.timer = setInterval(() => this.render(), 1000)
+      this.render();
+      this.timer = setInterval(() => this.render(), 1000);
    }
 }
 let clock = new Clock({ template: 'h:m:s' });
@@ -982,8 +983,8 @@ function sum(){
    let b = +prompt("Введите число еще раз");
    return a + b
 }
-console.log(sum());*/
-const storeName = "Fortuna"
+console.log(sum());
+const storeName = "Fortuna";
 const stopeDescription = {
    budget: 10000,
    employees: ['Kirill', 'Andrei', 'Maxim'],
@@ -993,11 +994,10 @@ const stopeDescription = {
    },
    open: true,
 };
-const answers = []
-answers[0] = prompt('Как вас зовут?')
-answers[1] = prompt('Какая ваша фамилия?')
-answers[2] = prompt('Сколько вам лет?')
+const answers = [];
+answers[0] = prompt('Как вас зовут?');
+answers[1] = prompt('Какая ваша фамилия?');
+answers[2] = prompt('Сколько вам лет?');
 answers.forEach(function(item, i, arr){
    console.log(item);
-})
-let a = b
+});*/
